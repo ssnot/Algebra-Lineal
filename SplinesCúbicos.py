09f7e6990826bc2a06c3a590c0c828540f1d775b
ssnot <adrianleon3005@gmail.com>
2026-04-13 22:14:06 -0500

    Update SplinesCúbicos.py

--- SplinesCúbicos.py
+++ SplinesCúbicos.py
@@ -1,1 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+"""
+Si no tienen las librerias, instálenlas con estos comandos en el cmd:
+pip install matplotlib
+pip install numpy
+"""
 
+coordsX = [0, 1, 2, 3]
+coordsY = [0, 1, 16, 81]
+
+n = len(coordsX) - 1
+
+Ak = []
+Bk = []
+Ck = []
+Dk = []
+
+gk = []
+hk = []
+for k in range(0, n):
+    hk.append(coordsX[k+1] - coordsX[k])
+
+print(hk)
+
+plt.plot(coordsX, coordsY)
+plt.show()
